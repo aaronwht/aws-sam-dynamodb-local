2346b527fb40d5bf013c074d904a50f6805694fc
Aaron White <awhite@tarvold.com>
2019-08-30 13:26:39 -0700

    Added dynamodb region

--- index.js
+++ index.js
@@ -4,14 +4,15 @@ const AWS = require('aws-sdk')
 AWS.config.update({
     dynamodb: {
         endpoint: process.env.AWS_DYNAMODB_ENDPOINT
-    }
+    },
+    region: "us-west-2"
 })
+
 
 // sam local start-api
 const clientdb = new AWS.DynamoDB.DocumentClient()
 
 module.exports.handler = async (event, context) => {
-   
     try {
         const persons = await clientdb.scan({ TableName: "Persons" }).promise()
         return {
