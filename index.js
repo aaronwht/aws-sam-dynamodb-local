c224b6462449fcab623b00543148b3ea71405f3d
Aaron White <awhite@tarvold.com>
2019-08-31 10:50:17 -0700

    Updates

--- index.js
+++ index.js
@@ -3,17 +3,14 @@ const AWS = require('aws-sdk')
 
 AWS.config.update({
     dynamodb: {
-        endpoint: process.env.AWS_DYNAMODB_ENDPOINT
-    },
-    region: "us-west-2"
+        endpoint: process.env.AWS_DYNAMODB_ENDPOINT,
+        region: 'us-west-2'
+    }
 })
 
-
 // sam local start-api
-// sam package --template-file template.yml --output-template-file package.yml --s3-bucket aws-lambda-dynamodb
-// sam deploy --template-file package.yml --stack-name lambda-dynamodb --capabilities CAPABILITY_IAM
 
-const clientdb = new AWS.DynamoDB.DocumentClient()
+const clientdb = new AWS.DynamoDB.DocumentClient(config)
 
 module.exports.handler = async (event, context) => {
     try {
